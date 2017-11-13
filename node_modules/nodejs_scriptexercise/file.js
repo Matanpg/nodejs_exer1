4abfa275471f016e8b43b21c4e6e30f2ec104c09
Matan Peleg <matanpg2@gmail.com>
2017-11-13 20:29:32 +0200

    fix

---
+++ node_modules/nodejs_scriptexercise/file.js
@@ -0,0 +1,23 @@
+
+function add(number1, number2) {
+    console.log(number1+number2);
+}
+
+function subtract(number1, number2) {
+    console.log(number1-number2);
+}
+
+function multiply(number1, number2) {
+    console.log(number1*number2);
+}
+
+function divide(number1, number2) {
+    console.log(number1/number2);
+}
+
+module.exports = {
+    "add": add,
+    "subtract": subtract,
+    "multiply": multiply,
+    "subtract": subtract,
+}
---
+++ test1.js
@@ -0,0 +1,3 @@
+const banana = require('nodejs_scriptexercise');
+
+console.log(banana.add(1,2));
